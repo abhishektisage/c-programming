23eae5434a169c69b9244691bc6fd513431cd726
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2024-06-06 22:59:21 +0530

    Add files via upload

---
+++ basic programs/swap.c
@@ -0,0 +1,14 @@
+//wap to swap / interchange values of 2 integer
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+     printf("enter any two number number:-\n");
+     scanf("%d %d",&a,&b);
+     c=a;
+     a=b;
+     b=c;
+     printf("after swapping the numbers are %d and %d",a,b);
+}
+	
+
